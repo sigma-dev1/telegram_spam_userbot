4222ce3c187a77eaf196676c6240c792761e4138
sigma-dev1 <132310748+sigma-dev1@users.noreply.github.com>
2024-07-24 02:52:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-api_id = env.int("API_ID")
-api_hash = env.str("API_HASH")
-phone_number = env.str("PHONE")
+api_id = env.int("28303272")
+api_hash = env.str("39bb8e386ca54538ae031dd514fb6552")
+phone_number = env.str("+393714099692")
